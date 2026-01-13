0405d5b47d77432e8132dc04d79d3076c360d00d
jellybee02 <wogus4039@naver.com>
2026-01-13 09:20:48 +0900

    router_update_1

--- Langgraph_Research/project1/src/main.py
+++ Langgraph_Research/project1/src/main.py
@@ -19,3 +19,13 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=8001, workers=2, reload=True, access_log=False, timeout_keep_alive = 120
+    )
+
+
+
